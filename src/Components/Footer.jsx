4f85396a0ad48728f8fb09a0f1f5b1d9c750264d
JANAKIRAMAN <janakiramankeerthivelan1021@gmail.com>
2025-01-11 14:45:28 +0530

    Frontend

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,43 +11,11 @@ const Footer = () => {
       <div className="mx-10 px-4 sm:px-6 lg:px-8 py-12 border-t border-gray-200">
         <div className="flex flex-col lg:flex-row justify-between items-center lg:items-start space-y-8 lg:space-y-0">
           <div className="flex flex-row items-center sm:items-start">
-            <img
-              src={logo}
-              alt="SecureDApp Logo"
-              className="h-12"
-            />
-            <h1 className="mt-3 ml-1 text-black dark:text-white text-lg font-bold">SecureDApp</h1>
+          
+            <h1 className="mt-3 ml-1 text-black dark:text-white text-lg font-bold">Secure3</h1>
           </div>
           <div className="grid grid-cols-2 sm:grid-cols-4 gap-8 text-sm">
-            <div>
-              <h3 className="font-bold text-black dark:text-white text-lg">Product</h3>
-              <ul className="mt-4 space-y-2 text-black dark:text-white text-md">
-                <li>Solidity Shield Scan</li>
-                <li>Secure Watch</li>
-                <li>Audit Express</li>
-                <li>Secure Trace</li>
-                <li>Secure Pad</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="font-bold text-black dark:text-white text-lg">Services</h3>
-              <ul className="mt-4 space-y-2 text-black dark:text-white text-md">
-                <li>Audit</li>
-                <li>Security</li>
-                <li>Regulatory Solutions</li>
-                <li>Training & Education</li>
-              </ul>
-            </div>
-            <div>
-              <h3 className="font-bold text-black dark:text-white text-lg">Company</h3>
-              <ul className="mt-4 space-y-2 text-black dark:text-white text-md">
-                <li>About Us</li>
-                <li>Authors</li>
-                <li>Media</li>
-                <li>Career</li>
-                <li>Contact Us</li>
-              </ul>
-            </div>
+          
           </div>
         </div>
         <div className="mt-8 flex justify-center lg:justify-start space-x-6">
